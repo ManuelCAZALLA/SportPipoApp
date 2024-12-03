55ee7b7aa1ed285da0390ffdd7fbee68107fb020
ManuelCAZALLA <manuelcm23@hotmail.com>
2024-12-03 20:12:33 +0100

    [Creo el Proyecto SportPipoApp]

---
+++ SportPipoAppTests/SportPipoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportPipoAppTests.swift
+//  SportPipoAppTests
+//
+//  Created by Manuel Cazalla Colmenero on 3/12/24.
+//
+
+import Testing
+@testable import SportPipoApp
+
+struct SportPipoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
