e0aae5505a8d7d49eb30e1b20bcebe19e9978937
ManuelCAZALLA <manuelcm23@hotmail.com>
2024-12-06 19:48:10 +0100

    [Arreglo error de clases de Coredata] arreglado error de coredata

--- SportPipoApp/Data/CoreData/ContentView.swift
+++ SportPipoApp/Data/CoreData/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  SportPipoApp
 //
-//  Created by Manuel Cazalla Colmenero on 3/12/24.
+//  Created by Manuel Cazalla Colmenero on 6/12/24.
 //
 
 import SwiftUI
@@ -14,16 +14,16 @@ struct ContentView: View {
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<NSManagedObject>
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
+                ForEach(items, id: \.self) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Item at \(item.value(forKey: "timestamp") as? Date ?? Date(), formatter: itemFormatter)")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text(item.value(forKey: "timestamp") as? Date ?? Date(), formatter: itemFormatter)
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -44,14 +44,12 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newItem = NSManagedObject(entity: Item.entity(), insertInto: viewContext)
+            newItem.setValue(Date(), forKey: "timestamp")
 
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -65,8 +63,6 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -80,7 +76,3 @@ private let itemFormatter: DateFormatter = {
     formatter.timeStyle = .medium
     return formatter
 }()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-}
