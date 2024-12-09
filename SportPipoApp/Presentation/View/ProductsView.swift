01b87974a68b0c57f216bbe8bb16775fd5bddb5c
ManuelCAZALLA <manuelcm23@hotmail.com>
2024-12-09 19:35:25 +0100

    [Coredata] configuro coredata

--- SportPipoApp/Presentation/View/ProductsView.swift
+++ SportPipoApp/Presentation/View/ProductsView.swift
@@ -8,46 +8,30 @@
 import SwiftUI
 
 struct ProductsView: View {
-    var products: [Product]
+    var products: [ProductModel] // Recibe modelos simples de productos
 
     var body: some View {
-        TabView {
-            
-            ProductCategoryView(
-                categoryName: "Suplementos",
-                products: products.filter { $0.category == "Suplementos" }
-            )
-            .tabItem {
-                Image(systemName: "leaf.circle.fill")
-                Text("Suplementos")
-            }
-
-            
-            ProductCategoryView(
-                categoryName: "Accesorios",
-                products: products.filter { $0.category == "Accesorios" }
-            )
-            .tabItem {
-                Image(systemName: "sportscourt.fill")
-                Text("Accesorios")
-            }
-
-            
-            AmazonPurchaseView()
-                .tabItem {
-                    Image(systemName: "cart.fill")
-                    Text("Compra")
+        List(products) { product in
+            VStack(alignment: .leading) {
+                Text(product.name)
+                    .font(.headline)
+                Text("Precio: \(product.price, specifier: "%.2f") €")
+                    .font(.subheadline)
+                if let descripcion = product.descripcion {
+                    Text(descripcion)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
                 }
+            }
         }
-        .accentColor(.blue)
+        .navigationTitle("Productos")
     }
 }
 
-
 #Preview {
-    ProductsView(products: [Product(name: "Creatina", price: 29.99, descripcion: "Mejora tu rendimiento.", category: "Suplementos", imageName: "cube.box.fill", linkAmazon: "https://amazon.com/creatina"),
-                            Product(name: "Proteínas", price: 19.99, descripcion: "Recuperación muscular.", category: "Suplementos", imageName: "flame.fill", linkAmazon: "https://amazon.com/proteinas"),
-                            Product(name: "Shaker", price: 9.99, descripcion: "Mezclador de calidad.", category: "Accesorios", imageName: "bottle.fill", linkAmazon: "https://amazon.com/shaker")
-                        ]
-)
-}
+    ProductsView(products: [
+                ProductModel(name: "Creatina", price: 29.99, descripcion: "Mejora tu rendimiento.", category: "Suplementos", imageName: "cube.box.fill", linkAmazon: "https://amazon.com/creatina"),
+                ProductModel(name: "Proteínas", price: 19.99, descripcion: "Recuperación muscular.", category: "Suplementos", imageName: "flame.fill", linkAmazon: "https://amazon.com/proteinas"),
+                ProductModel(name: "Shaker", price: 9.99, descripcion: "Mezclador de calidad.", category: "Accesorios", imageName: "bottle.fill", linkAmazon: "https://amazon.com/shaker")
+            ])
+        }
